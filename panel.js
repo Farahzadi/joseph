cdce5786091e4d6a65bd1e9db49f70431ce62a9d
Navid <na.farahzadi@gmail.com>
2025-05-26 13:21:07 +0330

    decode events

--- panel.js
+++ panel.js
@@ -1,4 +1,25 @@
+function decode(event) {
+  return JSON.parse(
+    webengage.util.compress.decompressFromBase64(
+      webengage.util.transit.decode(event)
+    )
+  );
+}
 document.getElementById("inspectBtn").addEventListener("click", () => {
+  chrome.devtools.network.getHAR((e) => {
+    const data = e.entries
+      .filter((item) => item._resourceType === "ping")
+      .map((elem) => elem.request.postData)
+      .filter(Boolean)
+      .map((elem) => elem.text);
+
+    console.log(data);
+    const encodedEvent = data
+      .map(JSON.parse)
+      .map((elem) => elem.data)
+      .map(decode);
+    console.log(encodedEvent);
+  });
   chrome.devtools.inspectedWindow.eval(
     "document.title",
     (result, isException) => {
