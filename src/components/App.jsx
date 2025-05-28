9e3692ede65448384a439047259306ddd45f60e4
Navid <na.farahzadi@gmail.com>
2025-05-28 17:33:09 +0330

    decode and show webengage events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,37 @@
-function decode(event) {
-  return JSON.parse(
-    webengage.util.compress.decompressFromBase64(
-      webengage.util.transit.decode(event)
-    )
-  );
-}
+import { createSignal, For } from "solid-js";
+import DevtoolsListener, { events } from "./DevtoolsListener";
 
-let events = [];
-chrome.devtools.network.onRequestFinished.addListener(function (req) {
-  if (req.request.url === "https://c.webengage.com/l4.jpg") {
-    const { data } = JSON.parse(req.request.postData.text);
-    events = _.chain(events).concat(decode(data)).value();
-    console.log(events);
-  }
-});
+// function decode(event) {
+//   return JSON.parse(
+//     webengage.util.compress.decompressFromBase64(
+//       webengage.util.transit.decode(event)
+//     )
+//   );
+// }
+
+// let events = [];
+// chrome.devtools.network.onRequestFinished.addListener(function (req) {
+//   if (req.request.url === "https://c.webengage.com/l4.jpg") {
+//     const { data } = JSON.parse(req.request.postData.text);
+//     events = _.chain(events).concat(decode(data)).value();
+//     console.log(events);
+//   }
+// });
 
 export default () => {
   return (
-    <h1 className="text-2xl text-pink-300 font-bold text-center mt-10">
-      Joseph is listening...
-    </h1>
+    <div className="container mx-auto">
+      <DevtoolsListener />
+      <h1 className="text-2xl text-pink-300 font-bold text-center mt-10">
+        Joseph is listening...
+      </h1>
+      <For each={events()}>
+        {(event, index) => (
+          <li>
+            Event {index() + 1}: {JSON.stringify(event)}
+          </li>
+        )}
+      </For>
+    </div>
   );
 };
---
+++ src/components/DevtoolsListener.jsx
@@ -0,0 +1,31 @@
+import { onMount, onCleanup, createSignal } from "solid-js";
+import _ from "lodash";
+
+export const [events, setEvents] = createSignal([]);
+
+function decode(event) {
+  return JSON.parse(
+    webengage.util.compress.decompressFromBase64(
+      webengage.util.transit.decode(event)
+    )
+  );
+}
+
+export default function DevtoolsListener() {
+  onMount(() => {
+    function handleRequest(req) {
+      if (req.request.url === "https://c.webengage.com/l4.jpg") {
+        const { data } = JSON.parse(req.request.postData.text);
+        setEvents((prev) => _.concat(prev, decode(data)));
+      }
+    }
+
+    chrome.devtools.network.onRequestFinished.addListener(handleRequest);
+
+    onCleanup(() => {
+      chrome.devtools.network.onRequestFinished.removeListener(handleRequest);
+    });
+  });
+
+  return null;
+}
